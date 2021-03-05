89758c7454bda4ad7a1f909b706e2e671384cdc6
jeongminhong <azurestefan@gmail.com>
2021-03-05 23:52:39 +0900

    firstcommit

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Nav, NavbarContainer } from './Navbar.elements';
+
+const Navbar = () => {
+  return (
+    <>
+      <Nav>
+        <NavbarContainer>{/* <NavLogo to='/'>
+            <NavIcon />
+            ULTRA
+          </NavLogo> */}</NavbarContainer>
+      </Nav>
+    </>
+  );
+};
+
+export default Navbar;
